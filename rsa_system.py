fb2812670f0ba710ea01a41fc36d035575b66d20
Taylor Thomas <stth223@uky.edu>
2022-04-14 14:53:51 -0400

    finish implementations of encrypt decrypt

--- rsa_system.py
+++ rsa_system.py
@@ -1,7 +1,9 @@
 import random
 import math
 import os
+from bitarray.util import int2ba
 
+# Python 3.9
 # Author: Sean Taylor Thomas
 # 4/22
 # Implementation of an RSA Cryptosystem
@@ -12,38 +14,31 @@ def is_coprime(a, b):
     return math.gcd(a, b) == 1
 
 
-def compute_e(p, q):
-    """ compute e and make sure e and (p-1)(q-1) are co-prime"""
+def generate_e(p, q):
+    """ generate e and make sure e and (p-1)(q-1) are co-prime"""
     e = 2 ** 16 + 1
     if is_coprime(e, (p - 1) * (q - 1)):
         return e
-    else:
-        print("E IS NOT CO-PRIME.... ):")
+    else:  # generate an e relatively prime to (p-1)(q-1)
+        e = generate_large_prime(17, 228)
+        while not is_coprime(e, (p - 1) * (q - 1)):
+            e = generate_large_prime()
+        return e
 
 
-def test_thing():
-    # for i in range(400):
-    #     if 2 ** i > 10**95:
-    #         print(i)
-    # random.randint(4, 5)
-    return 0
-
-def generate_large_int(p=-1):
+def generate_large_int(min_bits=333, max_bits=1200, p=-1):
     """
     returns: rand_int (random integer with k bits)
     optional argument: p, an int where |rand_int - p| > 10^95
     minimum k: 333 (2^333>10^100)
     """
-    min_bits = 333  # make sure number > 10^100
-    max_bits = 2500
 
     # if we already have first prime, make sure difference btwn primes is large
     if p != -1:
-        valid_bits_interval = [0, 0, 0, 0]  # min1, max1, min2, max2
         ks = [-1, -1]  # k's generated from 2 intervals (higher and lower than p)
 
         p_bits = p.bit_length()
-        valid_bits_interval = [min_bits, p_bits - 316, p_bits + 316, max_bits]
+        valid_bits_interval = [min_bits, p_bits - 316, p_bits + 316, max_bits]  # min1, max1, min2, max2
         # make sure range is valid
         if valid_bits_interval[1] - valid_bits_interval[0] > 0 and valid_bits_interval[3] - valid_bits_interval[2] > 0:
             ks[0] = random.randint(valid_bits_interval[0], valid_bits_interval[1])
@@ -64,26 +59,27 @@ def generate_large_int(p=-1):
     return rand_int
 
 
-def primality_test(x):
+def primality_test(n):
     """
     takes in large integer
     returns: whether the int passes the primality test
     """
-    n = 2**2500
-    return modular_exponentiation(x, n-1, n) == 1
+    a = random.randint(1, n)
+
+    return modular_exponentiation(a, n - 1, n) == 1
 
 
-def generate_large_prime(p=-1):
+def generate_large_prime(min_bits=333, max_bits=1200, p=-1):
     """
     gets large integer and checks if it passes the primality test
     returns: (probably) prime int
     """
-    x = generate_large_int(p)
-    passes_test = primality_test(x)
+    rand_int = generate_large_int(min_bits=min_bits, max_bits=max_bits, p=p)
+    passes_test = primality_test(rand_int)
     while not passes_test:
-        x = generate_large_int(p)
-        passes_test = primality_test(x)
-    return x
+        rand_int = generate_large_int(min_bits=min_bits, max_bits=max_bits, p=p)
+        passes_test = primality_test(rand_int)
+    return rand_int
 
 
 def modular_exponentiation(x, y, n):
@@ -92,18 +88,22 @@ def modular_exponentiation(x, y, n):
     arguments: x, y, n such that 0 <= x, y, <= n-1
     returns result of x^y (mod n)
     """
+    y_bits = int2ba(y)  # convert y into bit representation
+    y_bits.reverse()  # reverse this bit representation
     z = 1
-    for i in reversed(range(y.bit_length())):
-        z = z**2 % n
-        if y % 2 == 0:
-            z = x*z % n
+    for i in reversed(range(len(y_bits))):
+        z = z * z % n
+        if y_bits[i] == 1:
+            z = z * x % n
     return z
 
 
 def eea(e, n):
     """
     recursive implementation of extended euclid algorithm
-    for equation e (mod n) , solve e*x + n*y = gcd(e, n)
+    arguments: [ e*x + n*y = gcd(e, n) ]
+    1) e
+    2) n
     returns: (gcd, x, y)
     """
     if e == 0:
@@ -113,10 +113,18 @@ def eea(e, n):
         return gcd, y - (n // e) * x, x
 
 
+def get_positive_x(x, e, n):
+    while x < 0:
+        x += n
+    for i in range(random.randint(0, 12)):
+        x += n
+    return x
+
+
 def write_to_file(text="nothing to write", filename="default.txt"):
     """ arguments:
-        - text: text to write to file
-        - filename: name of file to write to
+        1) text: text to write to file
+        2) filename: name of file to write to
         """
     if os.path.exists(filename):
         with open(filename, 'w') as f:
@@ -126,10 +134,22 @@ def write_to_file(text="nothing to write", filename="default.txt"):
             f.write(text)
 
 
+def read_file(filename):
+    """
+    arguments:
+    1) filename (str) : what file to read from
+    returns: the contents of the file (str)
+    """
+    if os.path.exists(filename):
+        with open(filename, 'r') as f:
+            return f.read()
+    else:
+        print("Sorry, ", filename, " doesn't exist in this directory.")
+
+
 class rsa_system:
 
     def __init__(self):
-        self.generate_keys()
         self.public_key = ""
         self.private_key = ""
         self.p = -1
@@ -140,13 +160,16 @@ class rsa_system:
 
     def generate_keys(self):
         print("Keys being generated")
-        print("_" * 100)
+
 
         # generate public_key and write to file
+        print("_" * 100)
         self.generate_public_key()
         print("_" * 100)
+        print()
 
         # generate private_key an write to file
+        print("_" * 100)
         self.generate_private_key()
         print("_" * 100)
 
@@ -154,7 +177,7 @@ class rsa_system:
         print("Generating Public Key..")
         self.p = generate_large_prime()
         print("p generated: ", self.p)
-        self.q = generate_large_prime(self.p)
+        self.q = generate_large_prime(p=self.p)
 
         # make sure difference btwn p and q is large
         valid_q = abs(self.p - self.q) > 10 ** 95
@@ -164,21 +187,26 @@ class rsa_system:
             valid_q = abs(self.p - self.q) > 10 ** 95
         print("q generated: ", self.q)
 
-        # calculate n and compute e
+        # calculate n and generate e
         self.n = self.p * self.q
-        self.e = compute_e(self.p, self.q)
-        print("e computed: ", self.e)
+        self.e = generate_e(self.p, self.q)
+        print("e generated: ", self.e)
         self.public_key = str(self.n) + "\n" + str(self.e)
         print("Public Key: (", self.n, ", ", self.e, ")")
         write_to_file(text=self.public_key, filename="public_key.txt")
 
     def generate_private_key(self):
+        """
+        Generates a private key from the given public key information
+        """
         print("Generating Private Key..")
         gcd, x, y = eea(self.e, ((self.p - 1) * (self.q - 1)))
         print("Using EEA for e^(-1) mod (p-1)(q-1)...")
         print("GCD: ", gcd)
         print("x = ", x)
         print("y = ", y)
+        if x < 0:  # make x p positive if not already
+            x = get_positive_x(x, self.e, ((self.p - 1) * (self.q - 1)))
         self.d = x  # modular multiplicative inverse of e (mod (p-1)(q-1))
         self.private_key = str(self.d)
         write_to_file(text=self.private_key, filename="private_key.txt")
@@ -186,9 +214,36 @@ class rsa_system:
         print("d * e (mod (p-1)(q-1) = ", x * self.e % ((self.p - 1) * (self.q - 1)))
         print("Private Key ", self.d)
 
+    def encrypt(self):
+        """
+        Encrypts message found in message.txt
+        Writes ciphertext to ciphertext.txt file
+        """
+        # read in from files
+        public_key = read_file("public_key.txt").splitlines()
+        message = read_file("message.txt")
+
+        # compute ciphertext with modular exponentiation
+        ciphertext = str(modular_exponentiation(int(message), int(public_key[1]), int(public_key[0])))
+
+        write_to_file(text=ciphertext, filename="ciphertext.txt")
+
+    def decrypt(self):
+        """
+        Decrypts ciphertext given private key and public key
+        Writes decrypted message to decrypted_message.txt
+        """
+        public_key = read_file("public_key.txt").splitlines()
+        private_key = read_file("private_key.txt")
+        ciphertext = read_file("ciphertext.txt")
+
+        decrypted_message = str(modular_exponentiation(int(ciphertext), int(private_key), int(public_key[0])))
+        write_to_file(text=decrypted_message, filename="decrypted_message.txt")
 
 
-
-
-
-key = rsa_system()
+if __name__ == '__main__':
+    rsa = rsa_system()
+    rsa.generate_keys()  # generate public & private keys and output to files
+    # #
+    rsa.encrypt()  # encrypt message (from message.txt)
+    rsa.decrypt()  # decrypt the ciphertext generated from prev. steps
